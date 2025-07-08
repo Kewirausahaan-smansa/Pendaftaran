5e31dd754d4a0c01232a188824f298559d9954b5
Kewirausahaan-smansa <ahnad0311@gmail.com>
2025-07-09 01:41:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,19 +8,8 @@ document.getElementById('registrationForm').addEventListener('submit', function(
     const kelas = document.getElementById('kelas').value;
     const alasan = document.getElementById('alasan').value;
 
-    // Kirim data ke Google Sheets
-    fetch('https://script.google.com/macros/s/https://docs.google.com/spreadsheets/d/1Bt1q0OGfGVoHuOOQflfQLwX0U7uykNzgAfEiFVJDc3k/edit?usp=sharing/exec', {
-        method: 'POST',
-        body: JSON.stringify({ nama, email, telepon, kelas, alasan }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
         // Redirect ke grup WhatsApp
         window.location.href = "https://chat.whatsapp.com/DJ8BIXKM3rKq5ZPq4m4PN"; 
     })
-    .catch(error => console.error('Error:', error));
+    
 });
